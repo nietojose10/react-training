25f5522be7da49f9be3edeedab969ed49504bc5d
Jose <nietojose1995@gmail.com>
2023-11-01 14:53:11 -0600

    updated firstApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './App';
+import { CounterApp } from './firstApp/App';
 
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
